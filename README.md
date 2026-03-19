15d6370c1dcb9517fd5aa76a5ae67034b2246d3b
hiryamada <71864722+hiryamada@users.noreply.github.com>
2026-03-19 06:32:30 +0000

    init

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # customerstory
+
+- Azure Functionsリソースを作成
+- 一緒にできるストレージアカウントに以下を設定
+  - SecurityControl: Ignore タグを追加
+  - Configuration > Allow storage account key access: Enabled
+- GitHub Codespacesを起動
+- install-func.shを実行してAzure Functions Core Toolsをインストール
+- func init
+- func new
+- Azure API Managementリソースを作成 consumption
+- 
--- function_app.py
+++ function_app.py
@@ -19,9 +19,9 @@ def MyHttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(f"Hello!!! {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "Hello!!! This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
